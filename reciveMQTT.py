7a1c8ddd9070079e971346827d5fc996709530fb
lucasRivera21 <lucasriverar2001@gmail.com>
2022-11-18 17:35:51 -0500

    actualizacion

--- reciveMQTT.py
+++ reciveMQTT.py
@@ -13,7 +13,6 @@ conn = pymysql.connect(
 cur = conn.cursor()
 
 topics = [("salida/voltaje",0),("salida/osciloscopio",0)]
-output = None
 
 def on_connect(client, userdata, flags, rc):
         if rc == 0:
@@ -22,32 +21,31 @@ def on_connect(client, userdata, flags, rc):
             print("Failed to connect, return code %d\n", rc)
 
 def on_message(client, userdata, msg):
-    global output
     if msg.topic == "salida/voltaje":
         data = json.loads(msg.payload)
         output = data["voltajePagina"]
-        print(output)
-        query = f'INSERT INTO voltMult (voltaje) VALUES ({output})'
-        cur.execute(query)
-        cur.commit()
-        cur.close()
+        print(data["voltajePagina"])
+        query = f'INSERT INTO voltMult (voltaje) VALUES (%s)'
+        cur.execute(query,(data["voltajePagina"]))
+        conn.commit()
+        conn.close()
 
     elif msg.topic == "salida/osciloscopio":
         data = json.loads(msg.payload)
         output = data["voltajePagina"]
         print(output)
-        query = f'INSERT INTO voltOsc (voltaje) VALUES ({output})' 
-        cur.execute(query)
-        cur.commit()
-        cur.close()
+        # query = f'INSERT INTO voltOsc (voltaje) VALUES ({output})' 
+        # cur.execute(query)
+        # cur.commit()
+        # cur.close()
     elif msg.topic == "salida/corriente":
         data = json.loads(msg.payload)
         output = data["corrientePagina"]
         print(output)
-        query = f'INSERT INTO corrienteMult (corriente) VALUES ({output})'
-        cur.execute(query)
-        cur.commit()
-        cur.close()
+        # query = f'INSERT INTO corrienteMult (corriente) VALUES ({output})'
+        # cur.execute(query)
+        # cur.commit()
+        # cur.close()
 
 def listen():
     client = mqtt.Client()
